657672571650d6d628a83950a466ed838c09f424
pv <pat@cs.umass.edu>
2015-03-06 17:08:25 -0500

    try exporting result

--- src/main/scala/edu/umass/cs/iesl/UniversalSchemaPipeline.scala
+++ src/main/scala/edu/umass/cs/iesl/UniversalSchemaPipeline.scala
@@ -3,8 +3,10 @@ package edu.umass.cs.iesl
 import cc.factorie.app.nlp.Document
 import cc.factorie.app.nlp.ner.ConllChainNer
 import cc.factorie.app.nlp.pos.OntonotesForwardPosTagger
+import cc.factorie.app.nlp.relation.RelationMentionList
 import edu.umass.cs.iesl.entity_embeddings.EntityEmbeddingOpts
 import edu.umass.cs.iesl.entity_embeddings.data_structures.{ELDocument, English}
+import edu.umass.cs.iesl.tackbp2014.freebase.IdForm
 
 /**
  * Created by pv on 3/5/15.
@@ -27,6 +29,7 @@ object UniversalSchemaPipeline extends App
   val entityLinker = new EmbeddingEntiyLinking(opts)
   val exampleDoc = entityLinker.linkText(exampleDocumentText)
   RelationComponents.process1(exampleDoc)
+
   println("didnt crash")
 }
 
@@ -37,10 +40,32 @@ object TestRelationComponents extends App{
   val elDoc = ELDocument("test", exampleDocumentText,lang=English)
   // Convert to a factorie document
   val fDoc = elDoc.toFactorieDocument
-  // Define the mention finder
-//  OntonotesForwardPosTagger.process(fDoc)
-//  ConllChainNer.process(fDoc)
+
   EnglishNERMentionFinder.process(fDoc)
   RelationComponents.process1(fDoc)
-  println("didnt crash")
+//  println("didnt crash")
+  formatDoc(fDoc)
+
+  def formatDoc(doc: Document): String = {
+    val sb = new StringBuilder
+    val relationMentionList = doc.attr[RelationMentionList]
+    relationMentionList.foreach(rm => {
+      rm._relations.foreach(r => {
+        //        println("fb " +rm.arg1.entity.attr[FreebaseId])
+        //        println("wiki " +rm.arg1.entity.attr[WikipediaId])
+        //        println("sf "+rm.arg1.entity.attr[SurfaceFormId])
+
+        sb.append(s"${rm.arg1.entity.canonicalName}\t${rm.arg1.phrase.head.nerTag.baseCategoryValue}\t") // id1 nertag
+        sb.append(s"${rm.arg2.entity.canonicalName}\t${rm.arg2.phrase.head.nerTag.baseCategoryValue}\t") // id2 nertag
+        sb.append(s"${rm.arg1.string}\t${rm.arg2.string}\t") // string1 string2
+        sb.append(s"${doc.name}\t") // docid
+        sb.append(s"${rm.arg1.phrase.head.stringStart}-${rm.arg1.phrase.last.stringEnd}:") // first mention offsets
+        sb.append(s"${rm.arg2.phrase.head.stringStart}-${rm.arg2.phrase.last.stringEnd}:") // second mention offsets
+        sb.append(s"${rm.arg1.phrase.sentence.head.stringStart}-${rm.arg2.phrase.sentence.last.stringEnd}\t") // whole sentence offsets
+        sb.append(s"${r.provenance}") // evidence
+        sb.append("\n")
+      })
+    })
+    println(sb.toString)
+  }
 }
