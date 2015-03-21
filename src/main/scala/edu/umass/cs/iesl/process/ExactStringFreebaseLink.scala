c450320185aa9bd66bfc63d5a949f9226a411458
pv <pat@cs.umass.edu>
2015-03-21 12:58:05 -0400

    filter broken lines

--- src/main/scala/edu/umass/cs/iesl/process/ExactStringFreebaseLink.scala
+++ src/main/scala/edu/umass/cs/iesl/process/ExactStringFreebaseLink.scala
@@ -25,13 +25,19 @@ object ExactStringFreebaseLink extends ExactStringFreebaseLink("/home/pat/data/f
 
 abstract class ExactStringFreebaseLink(idNameFile : String) extends EntityLinker
 {   
-  val idNameMap = Source.fromFile(idNameFile).getLines().map { line =>
-//    val Array(fId, name) = line.split("\t")
-//    (name, fId)
-    val parts = line.split("\t")
-    println(parts.length, parts(0), parts(1))
-    (parts(0), parts(1))
-  }.toMap
+//  val idNameMap = Source.fromFile(idNameFile).getLines().map { line =>
+////    val Array(fId, name) = line.split("\t")
+////    (name, fId)
+//    val parts = line.split("\t")
+//    if (parts.length == 2)
+//      (parts(0), parts(1))
+//  }.toMap
+
+  val idNameMap = (for {
+    line <- Source.fromFile(idNameFile).getLines()
+    parts = line.split("\t")
+    if parts.length == 2
+  } yield (parts(0), parts(1))).toMap
 
   
   def linkName(name : String) : Option[String] ={
