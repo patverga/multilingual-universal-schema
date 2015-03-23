0aecabbe62a337b0e5ef3aa96b03ad5edd315961
pv <pat@cs.umass.edu>
2015-03-23 17:15:14 -0400

    batch process embedding entity linker

--- src/main/scala/edu/umass/cs/iesl/process/ProcessDataForUniversalSchema.scala
+++ src/main/scala/edu/umass/cs/iesl/process/ProcessDataForUniversalSchema.scala
@@ -24,14 +24,7 @@ object EmbeddingEntityLinkedProcesser extends ProcessDataForUniversalSchema
     val linker = initializeLinker(opts)
     val mentionFinder = if (opts.language.value == "es") SpanishNERMentionFinder else EnglishNERMentionFinder
 
-    val elDocs = IO.loadPlainTextTestData(opts)
-    val result = processELDocs(elDocs, mentionFinder, linker, opts.threads.value.toInt > 1)
-
-    println(result)
-
-    if (opts.outputFileName.wasInvoked) {
-      IO.exportStringToFile(opts.outputFileName.value, result)
-    }
+    batchProcess(opts, mentionFinder, linker)
   }
 
   def initializeLinker(opts : MultilingualUniversalSchemaOpts): LogisticRegressionTrainedLinker ={
@@ -81,28 +74,14 @@ object EmbeddingEntityLinkedProcesser extends ProcessDataForUniversalSchema
 
 object ExactStringFreebaseLinkedProcesser extends ProcessDataForUniversalSchema
 {
-  val batchSize = 256
-
   def main(args : Array[String]) {
     val opts = new MultilingualUniversalSchemaOpts
     opts.parse(args)
 
     val mentionFinder = if (opts.language.value == "es") SpanishNERMentionFinder else EnglishNERMentionFinder
-
-    val elDocs = IO.loadPlainTextTestData(opts)
-    var i = 0
-    while (i < elDocs.size) {
-      // hack to deal with ner lexicon oading not being threadsafe bug
-      val batch = if (i == 0) Seq(elDocs(0)) else elDocs.slice(i, Math.min(i+batchSize, elDocs.size))
-      val result = processELDocs(batch, mentionFinder, ExactStringFreebaseLink, opts.threads.value.toInt > 1)
-      println(result)
-      if (opts.outputFileName.wasInvoked) {
-        IO.exportStringToFile(opts.outputFileName.value, result, append=true)
-      }
-      i += batch.size
-    }
+    batchProcess(opts, mentionFinder, ExactStringFreebaseLink)
   }
-  
+
   def formatRelationsForExport(doc: Document): String = {
     val sb = new StringBuilder
     val relationMentionList = doc.attr[EntityLinkedRelationMentionList]
@@ -126,6 +105,23 @@ object ExactStringFreebaseLinkedProcesser extends ProcessDataForUniversalSchema
 
 abstract class ProcessDataForUniversalSchema
 {
+  val batchSize = 256
+
+  def batchProcess(opts: MultilingualUniversalSchemaOpts, mentionFinder: MultilingualNERMentionFinder,
+                   entityLinker : EntityLinker): Unit = {
+    val elDocs = IO.loadPlainTextTestData(opts)
+    var i = 0
+    while (i < elDocs.size) {
+      // hack to deal with ner lexicon oading not being threadsafe bug
+      val batch = if (i == 0) Seq(elDocs(0)) else elDocs.slice(i, Math.min(i + batchSize, elDocs.size))
+      val result = processELDocs(batch, mentionFinder, entityLinker, opts.threads.value.toInt > 1)
+      println(result)
+      if (opts.outputFileName.wasInvoked) {
+        IO.exportStringToFile(opts.outputFileName.value, result, append = true)
+      }
+      i += batch.size
+    }
+  }
 
   def processELDocs(elDocs : Seq[ELDocument], mentionFinder: MultilingualNERMentionFinder,
                     linker : EntityLinker, parallel : Boolean = true): String = {
