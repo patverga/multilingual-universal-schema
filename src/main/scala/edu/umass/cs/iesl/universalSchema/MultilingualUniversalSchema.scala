a9ed125a12e637963657afe307d5ba003aa12c41
pv <pat@cs.umass.edu>
2015-03-25 11:41:01 -0400

    make a kb creation from two files

--- src/main/scala/edu/umass/cs/iesl/universalSchema/MultilingualUniversalSchema.scala
+++ src/main/scala/edu/umass/cs/iesl/universalSchema/MultilingualUniversalSchema.scala
@@ -9,7 +9,8 @@ import scala.util.Random
  */
 
 class TrainTestMultilingualUniversalSchemaOptions extends cc.factorie.util.DefaultCmdOptions {
-  val tacData = new CmdOption("training-data", "", "FILE", "tab separated file with training data of the form : e1 e2 rel score")
+  val extractedData = new CmdOption("training-data", "", "FILE", "tab separated file with training data extracted from text of the form : e1 e2 rel score")
+  val freebaseData = new CmdOption("freebase-data", "", "FILE", "tab separated file with freebase data of the form : e1 e2 rel score")
   val dim = new CmdOption("dim", 100, "INT", "dimensionality of data")
   val testCount = new CmdOption("test-count", 1000, "INT", "number of cells to test")
   val stepsize = new CmdOption("stepsize", 0.1, "DOUBLE", "step size")
@@ -17,6 +18,7 @@ class TrainTestMultilingualUniversalSchemaOptions extends cc.factorie.util.Defau
   val useMaxNorm =  new CmdOption("use-max-norm", true, "BOOLEAN", "whether to use maximum l2-norm for vectors")
   val prune =  new CmdOption("prune", false, "BOOLEAN", "Prune non largest connected component")
   val regularizer = new CmdOption("regularizer", 0.01, "DOUBLE", "regularizer")
+  val encoding = new CmdOption("encoding", "UTF-8", "String", "encoding of input data")
 }
 
 
@@ -30,7 +32,8 @@ object TrainTestMultilingualUniversalSchema {
     opts.parse(args)
 
     val tReadStart = System.currentTimeMillis
-    var kb = EntityRelationKBMatrix.fromTsv(opts.tacData.value, colsPerEnt = 1)
+    var (kb, testCols) = MultilingualEntityRelationKBMatrix.fromTsv(
+      opts.extractedData.value, opts.freebaseData.value, encoding = opts.encoding.value, colsPerEnt = 1)
     if (opts.prune.wasInvoked) kb = kb.prune(2,1)
 
     val tRead = (System.currentTimeMillis - tReadStart)/1000.0
@@ -44,7 +47,7 @@ object TrainTestMultilingualUniversalSchema {
     val random = new Random(0)
     val numDev = 0
     val numTest = opts.testCount.value
-    val (trainKb, devKb, testKb) = kb.randomTestSplit(numDev, numTest, None, None, random)
+    val (trainKb, devKb, testKb) = kb.randomTestSplit(numDev, numTest, None, Some(testCols), random)
 
     val model = UniversalSchemaModel.randomModel(kb.numRows(), kb.numCols(), opts.dim.value, random)
 
@@ -81,4 +84,33 @@ object TrainTestMultilingualUniversalSchema {
     println("MAP after 200 iterations: " + Evaluator.meanAveragePrecision(result))
   }
 
+}
+
+object MultilingualEntityRelationKBMatrix {
+
+  private def entitiesAndRelFromLine(line: String, colsPerEnt:Int): (EntityPair, String, Double) = {
+    val parts = line.split("\t")
+    val e1 : String = parts.slice(0, colsPerEnt).mkString("\t")
+    val e2 : String = parts.slice(colsPerEnt, 2 * colsPerEnt).mkString("\t")
+    val rel : String = parts.slice(2 * colsPerEnt, parts.length - 1).mkString("\t")
+    val cellVal : Double = parts(parts.length - 1).toDouble
+    (EntityPair(e1, e2), rel, cellVal)
+  }
+  // Loads a matrix from a tab-separated file
+  def fromTsv(filename:String, freebaseFile:String, encoding : String = "UTF-8", colsPerEnt:Int = 2)
+  : (EntityRelationKBMatrix, Set[String]) = {
+    val kb = new EntityRelationKBMatrix()
+    scala.io.Source.fromFile(filename, encoding).getLines().foreach(line => {
+      val (ep, rel, cellVal) = entitiesAndRelFromLine(line, colsPerEnt)
+      kb.set(ep, rel, cellVal)
+    })
+    // get freebase relations as text columns
+    val testCols = Set[String]()
+    scala.io.Source.fromFile(freebaseFile, encoding).getLines().foreach(line => {
+      val (ep, rel, cellVal) = entitiesAndRelFromLine(line, colsPerEnt)
+      kb.set(ep, rel, cellVal)
+      testCols.+(rel)
+    })
+    (kb, testCols)
+  }
 }
