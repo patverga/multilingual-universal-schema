46bf30b14e1d1508bf55bdac1084360c1b4f9890
patverga <pat@cs.umass.edu>
2015-04-01 13:55:53 -0400

    see if this fixes the problem

--- py/split_arvind_data_by_pair.py
+++ py/split_arvind_data_by_pair.py
@@ -22,6 +22,16 @@ for (mention_pair, lines) in pair_lines.iteritems():
         index = 0 if i == 0 else randint(0, 2)
         splits[index].append(line)
 
+train_relations_set = set([line.split('\t')[1] for line in splits[0]])
+for i in range(1, 3):
+    not_in_train = {line.split('\t')[1]: line for line in splits[i] if line.split('\t')[1] not in train_relations_set}
+    train_relations_set.update(not_in_train.keys())
+    not_in_train_set = set(not_in_train.values())
+    splits[i] = [line for line in splits[i] if line not in not_in_train_set]
+    splits[0] += [line for line in splits[i] if line in not_in_train_set]
+
+print(len(splits[0]), len(splits[1]), len(splits[2]))
+
 for (split, name) in zip(splits, out_names):
     shuffle(split)
     f = open(name, 'w')
--- py/split_arvind_data_by_relation.py
+++ py/split_arvind_data_by_relation.py
@@ -22,6 +22,14 @@ for (relation, lines) in relation_lines.iteritems():
         index = 0 if i == 0 else randint(0, 2)
         splits[index].append(line)
 
+# need to make sure train set also contains each pair
+train_pairs_set = set([line.split('\t')[0] for line in splits[0]])
+for i in range(1, 2):
+    splits[i] = [line for line in splits[i] if line.split('\t')[0] not in train_pairs_set]
+    splits[0] += [line for line in splits[i] if line.split('\t')[0] in train_pairs_set]
+
+print(len(splits[0]), len(splits[1]), len(splits[2]))
+
 for (split, name) in zip(splits, out_names):
     shuffle(split)
     f = open(name, 'w')
