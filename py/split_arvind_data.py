cf60058b9a48927b8e582fdce87f7ce72a42fb69
patverga <pat@cs.umass.edu>
2015-04-02 13:42:21 -0400

    add negative samples to dev and test

--- py/split_arvind_data.py
+++ py/split_arvind_data.py
@@ -4,22 +4,33 @@ import sys
 
 
 def main(argv):
-    if(len(argv) < 1):
+    if len(argv) < 1:
         print ("must supply input file")
         sys.exit()
 
+    neg_samples = 10
     out_names = ["train", "dev", "test"]
     splits = [[], [], []]
     pair_set = set()
     relation_set = set()
 
-    # read lines into a map from mention pair -> lines containing that pair
+    # save all pairs first to negative sample
+    pairs = []
+    for line in open(argv[0], 'r'):
+        pairs.append(line.split('\t')[0])
+    shuffle(pairs)
+
     for line in open(argv[0], 'r'):
         parts = line.split('\t')
         pair = parts[0]
         relation = parts[1]
         index = 0 if pair not in pair_set or relation not in relation_set else randint(0, 2)
         splits[index].append(line)
+        # add negative samples
+        if index > 0:
+            for i in range(1, neg_samples):
+                splits[randint(1, 2)].append(pairs[randint(0, len(pairs) - 1)] + "\t" + relation + "\t0")
+
         if pair not in pair_set:
             pair_set.add(pair)
         if relation not in relation_set:
@@ -33,6 +44,7 @@ def main(argv):
         for line in split:
             f.write(line)
         f.close()
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
