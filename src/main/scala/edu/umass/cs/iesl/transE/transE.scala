ed860f7ccdae1256b2932409433fa36665a315d7
patverga <pat@cs.umass.edu>
2015-04-09 17:38:18 -0400

    transe works and l1 works

--- src/main/scala/edu/umass/cs/iesl/transE/transE.scala
+++ src/main/scala/edu/umass/cs/iesl/transE/transE.scala
@@ -19,19 +19,21 @@ import scala.util.Random
  */
 class TransE(val opts: TransEOpts) extends Parameters {
 
-  val D = 50
+  val D = 100
   var weights: Seq[Weights] = null
-  val gamma = 1
+  val gamma = 1.0
+  // use L1 distance, L2 otherwise
+  val l1 = true
 
-  protected val threads = 1
-  protected val adaGradDelta = 0
+  protected val threads = 4
+  protected val adaGradDelta = 0.0
   protected val adaGradRate = 0.01
   protected val encoding = "UTF-8"
 
   protected val minRelationCount = 1
   protected val negativeSamples = 1
-  protected val iterations = 10000
-  protected val miniBatchSize = 1000
+  protected val iterations = 1000
+  protected val numBatches = 100
 
   protected var trainer: Trainer = null
   protected var optimizer: GradientOptimizer = null
@@ -86,46 +88,61 @@ class TransE(val opts: TransEOpts) extends Parameters {
   }
 
   // Component-2
-  def learnEmbeddings(relationList: Seq[(String, String, String)]): Unit = {
+  def learnEmbeddings(trainTriplets: Seq[(String, String, String)]): Unit = {
     println("Learning Embeddings")
+    val batchSize = trainTriplets.size / numBatches
 
-//    optimizer = new AdaGradRDA(delta = adaGradDelta, rate = adaGradRate)
+    //    optimizer = new AdaGradRDA(delta = adaGradDelta, rate = adaGradRate)
     optimizer = new AdaGrad(delta = adaGradDelta, rate = adaGradRate)
-    trainer = new HogwildTrainer(weightsSet = this.parameters, optimizer = optimizer, nThreads = threads, maxIterations = Int.MaxValue)
-//    trainer = new OnlineTrainer(weightsSet = this.parameters, optimizer = optimizer, maxIterations = Int.MaxValue)
+//    trainer = new HogwildTrainer(weightsSet = this.parameters, optimizer = optimizer, nThreads = threads, maxIterations = Int.MaxValue)
+    trainer = new OnlineTrainer(weightsSet = this.parameters, optimizer = optimizer, maxIterations = Int.MaxValue, logEveryN = batchSize)
 
-//    weights = (0 until entityCount + relationCount).map(i => Weights(TensorUtils.setToRandom1(new DenseTensor1(D, 0)))) // initialized using wordvec random
-    //TODO make sure this works
-    weights = Seq.fill(entityCount + relationCount)(Weights(TensorUtils.setToRandom1(new DenseTensor1(D, 0)))) // initialized using wordvec random
+    weights = (0 until entityCount + relationCount).map(i => Weights(TensorUtils.setToRandom1(new DenseTensor1(D, 0)))) // initialized using wordvec random
     optimizer.initializeWeights(this.parameters)
 
     // normalize relation embeddings once
-    //TODO check bounds
-    (entityCount until relationCount + entityCount).par.foreach(weights(_).value.twoNormalize())
+    (entityCount until weights.size).par.foreach(weights(_).value.twoNormalize())
     println(weights.size, entityCount, entityVocab.size(), relationCount, relationVocab.size())
 
     for (iteration <- 0 to iterations) {
       println(s"Training iteration: $iteration")
-      // sample miniBatch
-      val miniBatch = Seq.fill(miniBatchSize)(relationList(rand.nextInt(relationList.size)))
       // normalize entity embeddings
-      // TODO check bounds
-//      for (i <- 0 to entityCount -1) weights(i).value.twoNormalize()
-      (0 until entityCount).par.foreach(weights(_).value.twoNormalize())
+//      (0 until entityCount).par.foreach(weights(_).value.twoNormalize())
+      // normalize relations : paper says they dont do this but makes results better
+      (0 until entityCount + relationCount).par.foreach(weights(_).value.twoNormalize())
 
-      processMiniBatch(miniBatch)
+      (0 until numBatches).foreach(batch => {
+        // sample random triplets for miniBatch
+        val miniBatch = Seq.fill(batchSize)(trainTriplets(rand.nextInt(trainTriplets.size)))
+        processMiniBatch(miniBatch)
+      })
     }
     println("Done learning embeddings. ")
     //store()
   }
 
-  def evaluate(relationList: Seq[(String, String, String)]): Unit = {
-    val headRanks = new ArrayBuffer[Int]
-    val tailRanks = new ArrayBuffer[Int]
-    var tripletIndex = 0
-    // rank each triplet in the set
-    while (tripletIndex < relationList.size) {
-      val (e1, relation, e2) = relationList(tripletIndex)
+  protected def processMiniBatch(relationList: Seq[(String, String, String)]): Unit = {
+    val examples = relationList.map { case (e1, relation, e2) =>
+      val e1Index = entityVocab.get(e1)
+      val e2Index = entityVocab.get(e2)
+      val relationIndex = relationVocab.get(relation) + entityCount
+      // corrupt either head or tail
+      if (rand.nextInt(2) == 0) new TansEExample(this, e1Index, relationIndex, e2Index, rand.nextInt(entityCount), e2Index, l1)
+      else new TansEExample(this, e1Index, relationIndex, e2Index, e1Index, rand.nextInt(entityCount), l1)
+    }
+    trainer.processExamples(examples)
+  }
+
+  /**
+   * for each test triplet, rank the correct answer amongst all corrupted head triplets
+   * and all corrupted tail triplets
+   * @param testTriplets test triples in form e1 relation e2
+   * @return (hits@10, averageRank)
+   */
+  def evaluate(testTriplets: Seq[(String, String, String)]): (Double, Double) = {
+
+    println(s"Evaluating on ${testTriplets.size} samples")
+    val ranks: Seq[Int] = testTriplets.par.flatMap { case (e1, relation, e2) =>
       val e1Id = entityVocab.get(e1)
       val e2Id = entityVocab.get(e2)
       val e1Emb = weights(e1Id).value
@@ -134,72 +151,39 @@ class TransE(val opts: TransEOpts) extends Parameters {
 
       var headRank = 0
       var tailRank = 0
-      val score = (e1Emb + relEmb - e2Emb).twoNorm
+      val posScore = if (l1) (e1Emb + relEmb - e2Emb).oneNorm else (e1Emb + relEmb - e2Emb).twoNorm
 
+      // store to save time
+      val e1Rel = e1Emb + relEmb
+      val relE2 = relEmb - e2Emb
+
+      // iterate over each other entity in dictionary
       var negativeId = 0
-      while (negativeId < entityVocab.size()) {
-        val negEmb = weights(negativeId).value
+      while (negativeId < entityCount) {
         // dont self rank
-        if(negativeId != e2Id && negativeId != e2Id) {
-          val negHeadScore = (e1Emb + relEmb - negEmb).twoNorm
-          if (negHeadScore < score)
+        if (negativeId != e1Id && negativeId != e2Id) {
+          val negEmb = weights(negativeId).value
+          val negHeadScore = if (l1) (e1Rel - negEmb).oneNorm else (e1Rel - negEmb).twoNorm
+          if (negHeadScore < posScore)
             headRank += 1
-          val negTailScore = (negEmb + relEmb - e2Emb).twoNorm
-          if (negTailScore < score)
+          val negTailScore = if (l1) (negEmb + relE2).oneNorm else (negEmb + relE2).twoNorm
+          if (negTailScore < posScore)
             tailRank += 1
         }
         negativeId += 1
       }
-      if (tripletIndex % 1000 == 0) println(tripletIndex.toDouble/relationList.size.toDouble, tailRank, headRank)
-      tailRanks += tailRank
-      headRanks += headRank
-      tripletIndex += 1
-    }
-    val ranks = tailRanks ++ headRanks
-    println(ranks.count(_ <= 10).toDouble / (relationList.size * 2).toDouble, ranks.sum / ranks.length)
-  }
-
-
-  protected def processMiniBatch(relationList: Seq[(String, String, String)]): Unit = {
-    val examples = relationList.map{case (e1, relation, e2) =>
-      val e1Index = entityVocab.get(e1)
-      val e2Index = entityVocab.get(e2)
-      val relationIndex = relationVocab.get(relation) + entityCount
-      // corrupt either head or tail
-      if (rand.nextInt(2) == 0) new TansEExample(this, e1Index, relationIndex, e2Index, rand.nextInt(entityCount), e2Index)
-      else new TansEExample(this, e1Index, relationIndex, e2Index, e1Index, rand.nextInt(entityCount))
-    }
-     trainer.processExamples(examples)
+      Seq(headRank, tailRank)
+    }.seq
+    // return hits@10 and avg rank
+    (ranks.count(_ < 10).toDouble / ranks.size.toDouble, ranks.sum / ranks.length)
   }
 }
-
-object TestTransE extends App {
-
-  val opts = new TransEOpts()
-  opts.parse(args)
-
-  val transE = new TransE(opts)
-//  val train = transE.buildVocab("/Users/pat/universal-schema/transe/SME/data/FB15k/freebase_mtr100_mte100-train.txt")
-//  val test = transE.buildVocab("/Users/pat/universal-schema/transe/SME/data/FB15k/freebase_mtr100_mte100-test.txt")
-  val train = transE.buildVocab(opts.train.value)
-  val test = transE.buildVocab(opts.test.value)
-  println(train.size, test.size)
-  transE.learnEmbeddings(train)
-  transE.evaluate(test)
-
-}
-
-class TransEOpts extends CmdOptions{
-  val train = new CmdOption[String]("train", "", "FILENAME", "Train file.")
-  val test = new CmdOption[String]("test", "", "FILENAME", "Test File.")
-  val outputFileName = new CmdOption[String]("output-filename", "", "FILENAME", "Output")
-}
-
 
 class TansEExample(model: TransE, e1PosDex: Int, relDex: Int, e2PosDex: Int, e1NegDex: Int, e2NegDex: Int, l1: Boolean = false) extends Example {
 
-  def accumulateValueAndGradient(value: DoubleAccumulator, gradient: WeightsMapAccumulator): Unit =
-  {
+  val factor: Double = 1.0
+
+  def accumulateValueAndGradient(value: DoubleAccumulator, gradient: WeightsMapAccumulator): Unit = {
     val e1PosEmb = model.weights(e1PosDex).value
     val e1NegEmb = model.weights(e1NegDex).value
 
@@ -210,27 +194,51 @@ class TansEExample(model: TransE, e1PosDex: Int, relDex: Int, e2PosDex: Int, e1N
 
     var posGrad = e2PosEmb - e1PosEmb - relEmb
     var negGrad = e2NegEmb - e1NegEmb - relEmb
-//    posGrad = if (l1 && posGrad > 0) 1 else if (l1 && posGrad <= 0) -1 else posGrad
 
     // gamma + pos - neg
     val objective = if (l1) model.gamma + posGrad.oneNorm - negGrad.oneNorm
     else model.gamma + posGrad.twoNorm - negGrad.twoNorm
 
-    val factor: Double = 1.0
+    if (l1) {
+      (0 until posGrad.size).foreach(i => {
+        posGrad(i) = if (posGrad(i) > 0) 1.0 else -1.0
+        negGrad(i) = if (negGrad(i) > 0) 1.0 else -1.0
+      })
+    }
 
     if (value ne null) value.accumulate(objective)
 
     // hinge loss
-    if (gradient != null && objective > 0.0)
-    {
+    if (gradient != null && objective > 0.0) {
       gradient.accumulate(model.weights(e1PosDex), posGrad, factor)
       gradient.accumulate(model.weights(e2PosDex), posGrad, -factor)
       gradient.accumulate(model.weights(relDex), posGrad, factor)
       gradient.accumulate(model.weights(e1NegDex), negGrad, -factor)
       gradient.accumulate(model.weights(e2NegDex), negGrad, factor)
       gradient.accumulate(model.weights(relDex), negGrad, -factor)
-//      gradient.accumulate(model.weights(relDex), e1PosEmb+e2NegEmb-e2PosEmb-e1NegEmb, factor)
 
     }
   }
 }
+
+object TestTransE extends App {
+
+  val opts = new TransEOpts()
+  opts.parse(args)
+
+  val transE = new TransE(opts)
+  val train = transE.buildVocab(opts.train.value)
+  val test = transE.buildVocab(opts.test.value)
+  println(train.size, test.size)
+  transE.learnEmbeddings(train)
+  println(transE.evaluate(test))
+
+}
+
+class TransEOpts extends CmdOptions {
+  val train = new CmdOption[String]("train", "", "FILENAME", "Train file.")
+  val test = new CmdOption[String]("test", "", "FILENAME", "Test File.")
+  val outputFileName = new CmdOption[String]("output-filename", "", "FILENAME", "Output")
+}
+
+
